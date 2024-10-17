d104dd6a1c79f4087ea4a052a1b2315707439653
Quan <qhoang2373@gmail.com>
2024-10-16 19:33:16 -0700

    Exercise 4 Done

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// <------ Exercuse 1 ----->
+
 // Use `.map()` to iterate over the following array:
 
 const nums = [13, 87, 2, 89, 12, 4, 90, 63];
@@ -10,6 +12,8 @@ const nums2 = nums.map((currentElement) => {
 
 console.log(nums2);
 
+// <------ Exercuse 2 ----->
+
 // Given the following array, use destructuring to pull out the first and second values and place them into variables. 
 // Log both variables.
 
@@ -17,8 +21,11 @@ const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
 
 const [firstTopping, secondTopping] = pizzaToppings;
 
-console.log(pizzaToppings[0]);
+console.log(firstTopping);
 console.log(secondTopping);
+
+
+// <------ Exercuse 3 ----->
 
 // Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
 
@@ -31,4 +38,18 @@ const { make, model } = car;
 
 console.log(car.make);
 console.log(car.model);
+
+
+// <------ Exercise 4 ----->
+
+// Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
+
+const pizzaToppers = ['Pineapple', 'Olives', 'Anchovies'];
+const duplicateToppings = [...pizzaToppers];
+
+console.log(pizzaToppers);
+
+
+// <------ Exercise 5 ----->
+
   
