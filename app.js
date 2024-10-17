999e7e149992c8cc3245712078cd3121a9cde620
Quan <qhoang2373@gmail.com>
2024-10-16 19:02:31 -0700

    test

--- app.js
+++ app.js
@@ -11,14 +11,4 @@ const nums2 = nums.map((currentNumber) => {
 console.log(nums2);
 
 
-// Use `.map()` to iterate over the following array:
-
-const nums = [13, 87, 2, 89, 12, 4, 90, 63];
-
-// Create a new array where each value is multiplied by 2 and log the new array.
-
-const nums2 = nums.map((currentNumber) => {
-    return currentNumber * 2;
-});
-
-console.log(nums2);
+tester
